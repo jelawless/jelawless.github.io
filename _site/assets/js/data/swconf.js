624f964b58b323792ee707785da077d4c1320d08
jelawless <jlawless1901@gmail.com>
2024-05-08 10:58:59 -0500

    added -site

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1715183760',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ],denyPaths: [ ], purge: false };
