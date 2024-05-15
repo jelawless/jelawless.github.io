eb2c7b0cc77a3fed739b77aa4ecc27098f1a327a
jelawless <jlawless1901@gmail.com>
2024-05-15 09:13:18 -0500

    Docs replace check

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,1 +1,6 @@
-const swconf = { cacheName: 'chirpy-1715183760',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ],denyPaths: [ ], purge: false };
+const swconf = {
+  
+    purge: true
+  
+};
+
